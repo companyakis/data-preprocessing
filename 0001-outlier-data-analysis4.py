8a7c9c117aaca3c77efd396ea3b0766cd1accc9b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-23 15:26:09 +0300

    Create 0001-outlier-data-analysis4.py

---
+++ 0001-outlier-data-analysis4.py
@@ -0,0 +1,17 @@
+#in this part, we'll use price info
+
+df_price = df[["price"]]
+
+print(df_price.describe())
+
+"""
+              price
+count  53940.000000
+mean    3932.799722
+std     3989.439738
+min      326.000000
+25%      950.000000
+50%     2401.000000
+75%     5324.250000
+max    18823.000000
+"""
